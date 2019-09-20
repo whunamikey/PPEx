d120280e8a6291357fb0db1d584c85194941e1af
shinaoo <125859320@qq.com>
2019-09-20 16:01:11 +0800

    增加main.cpp中server和client的启动函数

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 
 #include "socket/udp/UdpServer.h"
+#include "socket/udp/UdpClient.h"
+
+using namespace std;
+
+void startServer();
+void startClient();
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    UdpServer* udpServer = new UdpServer();
-    udpServer->start();
+
     return 0;
 }
+
+void startServer(){
+    cout << "start server" << endl;
+    UdpServer* udpServer = new UdpServer();
+    udpServer->start();
+}
+
+void startClient(){
+    cout << "start client" << endl;
+    UdpClient* udpClient =new  UdpClient();
+    udpClient->start();
+}
+
